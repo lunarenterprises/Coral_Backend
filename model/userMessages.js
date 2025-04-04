c09da7c0b3c113216afdb655c30ab8f0cf6d5ef0
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-04 10:23:03 +0530

    `Refactor SendMessage functionality in adminMessage.js and userMessages.js to use AdminSendMessage and UserSendMessage methods in model/userMessages.js`

--- model/userMessages.js
+++ model/userMessages.js
@@ -2,14 +2,19 @@ var db = require("../db/db");
 var util = require("util")
 const query = util.promisify(db.query).bind(db);
 
-module.exports.SendMessage = async (ticket_id, user_id, admin_id, message) => {
-    var Query = `insert into messages(ticket_id,user_id,admin_id,message)values(?,?,?,?)`;
-    return await query(Query, [ticket_id, user_id, admin_id, message]);;
+module.exports.AdminSendMessage = async (ticket_id, user_id, admin_id, message) => {
+    var Query = `insert into messages(ticket_id,user_id,admin_id,message,sendBy)values(?,?,?,?,?)`;
+    return await query(Query, [ticket_id, user_id, admin_id, message, admin_id]);;
+};
+
+module.exports.UserSendMessage = async (ticket_id, user_id, admin_id, message) => {
+    var Query = `insert into messages(ticket_id,user_id,admin_id,message,sendBy)values(?,?,?,?,?)`;
+    return await query(Query, [ticket_id, user_id, admin_id, message, user_id]);;
 };
 
 module.exports.ListMessages = async (ticket_id) => {
     var Query = `select * from messages where ticket_id=? `
-    return await query(Query,  [ ticket_id])
+    return await query(Query, [ticket_id])
 }
 
 module.exports.UpdateAdmin = async (user_id, ticket_id, to_admin_id) => {
