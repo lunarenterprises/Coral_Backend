919a99154d2a3d87282eda184718a3fb139c0b22
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-08 15:32:05 +0530

    `Update query in ListNotification.js to use 'created_at' instead of 'createdAt' in ORDER BY clause.`

--- model/ListNotification.js
+++ model/ListNotification.js
@@ -4,7 +4,7 @@ const query = util.promisify(db.query).bind(db);
 
 module.exports.getNotifications = async (user_id) => {
     // Step 1: Fetch notifications for the user
-    var fetchQuery = `SELECT * FROM notifications WHERE user_id = ? ORDER BY createdAt DESC`;
+    var fetchQuery = `SELECT * FROM notifications WHERE user_id = ? ORDER BY created_at DESC`;
     var notifications = await query(fetchQuery, [user_id]);
 
     if (notifications.length > 0) {
