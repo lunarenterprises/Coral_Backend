babeadfee6dc0ec74d1b29df9f8e90fa27011364
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-28 12:02:20 +0530

    `Added amount validation in Calculator controller`

--- model/terminatecontract.js
+++ model/terminatecontract.js
@@ -3,14 +3,8 @@ var util = require("util")
 const query = util.promisify(db.query).bind(db);
 
 module.exports.updateContract = async (ui_id, reason) => {
-    var Query = `insert into terminate_contract_req(tc_ui_id,tc_reason)values(?,?)`;
-    var data = await query(Query, [ui_id, reason]);
-    return data;
-};
-
-module.exports.isTerminated = async (ui_id) => {
-    var Query = `select * from terminate_contract_req where tc_ui_id = ?`;
-    var data = await query(Query, [ui_id]);
+    var Query = `update user_invest set ui_request=?,ui_request_reason=? where ui_id=?`;
+    var data = await query(Query, ["termination", reason, ui_id]);
     return data;
 };
 
@@ -19,12 +13,6 @@ module.exports.getusersdata = async (ui_id) => {
     inner join users on ui_u_id = u_id
     where ui_id = ?`;
     var data = await query(Query, [ui_id]);
-    return data;
-};
-
-module.exports.updateTerminationStatus = async (ui_id) => {
-    var queryUpdate = `UPDATE user_invest SET ui_status = ? WHERE ui_id = ?`;
-    let data = await query(queryUpdate, ['requestedForTermination', ui_id]);
     return data;
 };
 
