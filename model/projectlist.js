300f546c900cef75a5e40fbad819632b5b99d7af
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-29 14:22:13 +0530

    Updated model for project list

--- model/projectlist.js
+++ model/projectlist.js
@@ -3,7 +3,7 @@ var util = require("util")
 const query = util.promisify(db.query).bind(db);
 
 module.exports.getCompanyInvest = async () => {
-    var Query = `SELECT ri_project AS ci_industry, MIN(ri_amount_from) AS min_amount_from, MAX(ri_amount_to) AS max_amount_to, MIN(ri_return_year) AS earliest_return_year FROM return_invest GROUP BY ri_project ORDER BY min_amount_from ASC`;
+    var Query = `SELECT ri_project AS ci_industry, MIN(ri_id) AS ri_id, MIN(ri_amount_from) AS ri_amount_from, MAX(ri_amount_to) AS ri_amount_to, MIN(ri_return_year) AS ri_return_year FROM return_invest GROUP BY ri_project ORDER BY ri_amount_from ASC `;
     var data = await query(Query);
     return data;
 };
