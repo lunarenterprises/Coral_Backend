a2eef852e6a6f5b5d5a3ca8d65363da1409b545a
akhil_m1000 <akhilvinod76@gmail.com>
2025-04-29 14:26:37 +0530

    `Fixed editProfile controller: updated user id check, removed unused variable, and corrected model call`

--- controller/editProfile.js
+++ controller/editProfile.js
@@ -6,25 +6,24 @@ let notification = require('../util/saveNotification')
 module.exports.EditUserProfile = async (req, res) => {
     try {
         let { user_id } = req.headers
-        if(!user_id){
+        if (!user_id) {
             return res.send({
-                result:false,
-                message : "User id is required"
+                result: false,
+                message: "User id is required"
             })
         }
         let { u_currency, u_gender, u_address, u_mobile, u_country } = req.body
         let userData = await userModel.getUser(user_id)
-        if(userData.length===0){
+        if (userData.length === 0) {
             return res.send({
-                result:false,
+                result: false,
                 message: "User data not found"
             })
         }
-        if (!u_currency || (!userData[0]?.u_gender && !u_gender) || !u_address || !u_mobile || !u_country) {
+        if (!u_currency || !u_address || !u_mobile || !u_country) {
             return res.send({ result: false, message: "Please fill all the fields" })
         }
-        let gender = userData[0]?.u_gender ? userData[0]?.u_gender : u_gender
-        let update = await model.EditProfile(user_id, u_currency.toUpperCase(), gender, u_address, u_mobile, u_country)
+        let update = await model.EditProfile(user_id, u_currency.toUpperCase(), u_gender, u_address, u_mobile, u_country)
         if (update.affectedRows > 0) {
             await notification.addNotification(user_id, userData[0].u_role, "Profile Updated", "Your profile has been updated successfully")
             return res.send({
