5eeff514fc3d857563a009d167d38d0f6c1e1d96
akhil_m1000 <akhilvinod76@gmail.com>
2025-05-15 16:24:00 +0530

    `Refactored createPdfWithPuppeteer function with minor changes`

--- util/pdfGeneration.js
+++ util/pdfGeneration.js
@@ -1,33 +1,59 @@
 var puppeteer = require('puppeteer');
 const newPath = require('path');
 
-module.exports.createPdfWithPuppeteer = async function (htmlContent, path) {
+// module.exports.createPdfWithPuppeteer = async function (htmlContent, path) {
+//     try {
+//         let browser = await puppeteer.launch({
+//             args: ["--no-sandbox", "--disable-setuid-sandbox"],
+//             headless: true,
+//             // executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+//             executablePath: newPath.resolve(process.env.HOME, 'chromium/chrome-linux/chrome')
+//         });
+//         let page = await browser.newPage();
+
+//         await page.setContent(htmlContent, {
+//             waitUntil: "networkidle0",
+//             timeout: 60000,
+
+//         });
+
+//         await page.pdf({
+//             path: path,
+//             format: "A4",
+//             margin: {
+//                 top: "10px",
+//                 bottom: "10px",
+//                 left: "40px",
+//                 right: "40px",
+//             },
+//             printBackground: true,
+//             timeout: 0,
+//         });
+
+//         await browser.close();
+//     } catch (err) {
+//         console.error("Error_creating_PDF", err);
+//     }
+//     return path;
+// }
+
+module.exports.createPdfWithPuppeteer = async (htmlContent, path) => {
     try {
         let browser = await puppeteer.launch({
             args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            headless: true,
-            // executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+            headless: "new",
             executablePath: newPath.resolve(process.env.HOME, 'chromium/chrome-linux/chrome')
         });
         let page = await browser.newPage();
 
-        await page.setContent(htmlContent, {
-            waitUntil: "networkidle0",
-            timeout: 60000,
-
-        });
+        await page.setContent(htmlContent, { "waitUntil": "networkidle0" });
 
         await page.pdf({
             path: path,
             format: "A4",
-            margin: {
-                top: "10px",
-                bottom: "10px",
-                left: "40px",
-                right: "40px",
-            },
-            printBackground: true,
-            timeout: 0,
+            printBackground: true, // Only one instance of this key
+            displayHeaderFooter: true,
+            margin: { top: "50px", bottom: "50px" },
         });
 
         await browser.close();
