d0636e2343fcc689d2e4731aba6ea0633cc48a90
akhil_m1000 <akhilvinod76@gmail.com>
2025-05-19 11:25:17 +0530

    `Added company_id parameter to ListTopCompany function in model/top_company.js and updated controller/top_company.js to pass it`

--- controller/top_company.js
+++ controller/top_company.js
@@ -3,13 +3,14 @@ let model = require('../model/top_company')
 module.exports.ListTopCompany = async (req, res) => {
     try {
         let { user_id } = req.headers
+        let { company_id } = req.body
         if (!user_id) {
             return res.send({
                 result: false,
                 message: "User id is required"
             })
         }
-        let list = await model.ListTopCompany()
+        let list = await model.ListTopCompany(company_id)
         if (list.length) {
             return res.send({
                 result: true,
--- model/top_company.js
+++ model/top_company.js
@@ -2,7 +2,8 @@ var db = require("../db/db");
 var util = require("util")
 const query = util.promisify(db.query).bind(db);
 
-module.exports.ListTopCompany = async () => {
-    let Query = `select * from top_company where tc_status='active' `;
+module.exports.ListTopCompany = async (company_id) => {
+    let condition = company_id ? ` tc_id=${company_id} and ` : ``;
+    let Query = `select * from top_company where ${condition} tc_status='active' `;
     return await query(Query);
 }
