fc1ad3fc757e24d793cacbbac07e212abc2d678e
akhil_m1000 <akhilvinod76@gmail.com>
2025-07-05 10:13:29 +0530

    `Added fs and path dependencies and logged additional file system operations`

--- util/uploadFile.js
+++ util/uploadFile.js
@@ -1,11 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
+
 module.exports.saveFile = (oldPath, folder, filename) => {
     console.log(`📥 oldPath: ${oldPath}`);
     console.log(`📂 folder: ${folder}`);
     console.log(`📄 filename: ${filename}`);
-    
+
     const dirPath = path.join('/mnt/ebs500/uploads', folder);
     console.log(`🛠️ Creating dir: ${dirPath}`);
-    
+
     try {
         if (!fs.existsSync(dirPath)) {
             fs.mkdirSync(dirPath, { recursive: true });
@@ -17,7 +21,7 @@ module.exports.saveFile = (oldPath, folder, filename) => {
 
     const newPath = path.join(dirPath, filename);
     console.log(`📝 Writing to: ${newPath}`);
-    
+
     try {
         const fileData = fs.readFileSync(oldPath);
         fs.writeFileSync(newPath, fileData);
