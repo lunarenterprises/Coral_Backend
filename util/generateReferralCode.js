ad29d4f7985432448ce61323407b21abb14d5285
akhil_m1000 <akhilvinod76@gmail.com>
2025-07-23 12:16:13 +0530

    `Added payment method validation and wallet update functionality in AddOrder controller and model.`

--- util/generateReferralCode.js
+++ util/generateReferralCode.js
@@ -1,28 +1,12 @@
 const model = require('../model/users')
 
-const generateReferralCode = (length = 5) => {
+module.exports.generateUniqueReferralCode = async (length = 5) => {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghjklmnopqrstuvwxyz*-_+.';
     let code = '';
+    let time = Date.now()
     for (let i = 0; i < length; i++) {
         code += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-    return code;
-}
-
-module.exports.generateUniqueReferralCode = async (length = 5) => {
-    let code;
-    let exists = true;
-    let retries = 0;
-    const maxRetries = 5;
-
-    while (exists && retries < maxRetries) {
-        code = generateReferralCode(length);
-        exists = await model.isReferralCodeExists(code);
-        retries++;
-    }
-
-    if (exists) {
-        throw new Error("Failed to generate a unique referral code after multiple attempts");
-    }
+    code += time
     return code;
 }
