3c0d0b7f3ed6620e01b4a91be7c5876a4f0dc898
akhil_m1000 <akhilvinod76@gmail.com>
2025-08-18 16:55:50 +0530

    Updated the functionality to create bank for user

--- model/AddBank.js
+++ model/AddBank.js
@@ -2,9 +2,9 @@ var db = require("../db/db");
 var util = require("util")
 const query = util.promisify(db.query).bind(db);
 
-module.exports.Addbank = async (account_no, ifsc_code, swift_code, bank_name, branch_name, currency, user_id) => {
-    var Query = `insert into bank(b_u_id,b_account_no,b_ifsc_code,b_swift_code,b_name,b_branch,b_currency)values(?,?,?,?,?,?,?)`;
-    var data = await query(Query, [user_id, account_no, ifsc_code, swift_code, bank_name, branch_name, currency]);
+module.exports.Addbank = async (account_name,account_no, ifsc_code, swift_code, bank_name, branch_name, currency, user_id) => {
+    var Query = `insert into bank(b_name_as,b_u_id,b_account_no,b_ifsc_code,b_swift_code,b_name,b_branch,b_currency)values(?,?,?,?,?,?,?)`;
+    var data = await query(Query, [account_name,user_id, account_no, ifsc_code, swift_code, bank_name, branch_name, currency]);
     return data;
 };
 
